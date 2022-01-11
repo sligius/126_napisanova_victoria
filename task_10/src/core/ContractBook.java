5e9309312d2618a73f7222fff61526db2d5b8f8d
sligius <v.napisanova2003@gmail.com>
2022-01-11 12:12:26 +0300

    test complete

--- task_10/src/core/ContractBook.java
+++ task_10/src/core/ContractBook.java
@@ -1,6 +1,21 @@
 package core;
 
 public class ContractBook {
+    private int contractCount;
+    private ContractBook() {
+        contractCount = 0;
+    }
+
+    public void addCon(String number, String date) {
+        if (number == null && date == null) {
+            throw new IllegalArgumentException("number and date cannot be null");
+        }
+        if (number == null)
+            throw new IllegalArgumentException("number cannot be null");
+        if (date == null)
+            throw new IllegalArgumentException("date cannot be null");
+
+    }
 
     public int getConCount() {
         return 0;
--- task_10/src/tests/SystemTests.java
+++ task_10/src/tests/SystemTests.java
@@ -9,4 +9,28 @@ public class SystemTests extends Assert {
         ContractBook contractBook = ContractBook.create();
         assertEquals(0, contractBook.getConCount());
     }
+    @Test
+    public void addCon_AddConWithNumberAndDate_ConCountEqualsOne() {
+        ContractBook contractBook = ContractBook.create();
+        contractBook.addCon("number", "date");
+        assertEquals(0, contractBook.getConCount());
+    }
+    @Test
+    public void addCon_AddConWithNullNumber_ThrowsException() {
+        ContractBook contractBook = ContractBook.create();
+        var exc = assertThrows(IllegalArgumentException.class, () -> contractBook.addCon(null, "date"));
+        assertTrue(exc.getMessage().toLowerCase().contains("number cannot be null"));
+    }
+    @Test
+    public void addCon_AddConWithNullDate_ThrowsException() {
+        ContractBook contractBook = ContractBook.create();
+        var exc = assertThrows(IllegalArgumentException.class, () -> contractBook.addCon("number", null));
+        assertTrue(exc.getMessage().toLowerCase().contains("date cannot be null"));
+    }
+    @Test
+    public void addCon_AddConWithNullNumberAndNullDate_ThrowsException() {
+        ContractBook contractBook = ContractBook.create();
+        var exc = assertThrows(IllegalArgumentException.class, () -> contractBook.addCon(null, null));
+        assertTrue(exc.getMessage().toLowerCase().contains("number and date cannot be null"));
+    }
 }
