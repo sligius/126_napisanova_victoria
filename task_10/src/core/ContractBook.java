5a6fde186bc7c5b8e626406ba8af3f3f68602cfb
sligius <v.napisanova2003@gmail.com>
2022-01-11 11:09:59 +0300

    test complete

---
+++ task_10/src/core/ContractBook.java
@@ -0,0 +1,12 @@
+package core;
+
+public class ContractBook {
+
+    public int getConCount() {
+        return 0;
+    }
+
+    public static ContractBook create() {
+        return new ContractBook();
+    }
+}
--- task_10/src/tests/SystemTests.java
+++ task_10/src/tests/SystemTests.java
@@ -1,6 +1,12 @@
 package tests;
 
 import org.junit.*;
+import core.*;
 
 public class SystemTests extends Assert {
+    @Test
+    public void create_CreateEmptyContractBook_ConCountEqualsZero() {
+        ContractBook contractBook = ContractBook.create();
+        assertEquals(0, contractBook.getConCount());
+    }
 }
