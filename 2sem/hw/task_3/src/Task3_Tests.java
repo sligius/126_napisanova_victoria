5f9659eeebd41e264f67538c9ab8a587093a176c
sligius <v.napisanova2003@gmail.com>
2022-03-28 19:33:09 +0300

    add files

---
+++ 2sem/hw/task_3/src/Task3_Tests.java
@@ -0,0 +1,16 @@
+import org.junit.*;
+
+public class Task3_Tests extends Assert {
+
+    class TestComparator implements MyComparator<Integer> {
+        @Override
+        public int compare(Integer first, Integer second) {
+            return first - second;
+        }
+    }
+
+    /* QuickSort */
+
+    @Test
+    public void QuickSort_
+}
